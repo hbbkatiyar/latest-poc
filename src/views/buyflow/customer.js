bd58b5ab7c0d3ed48e4657de9c7d6bf2753be636
bk <binit.katiyar@toffeeinsurance.com>
2024-01-15 21:36:13 +0530

    Made changes in customer details section

--- src/views/buyflow/customer.js
+++ src/views/buyflow/customer.js
@@ -443,17 +443,18 @@ function CustomerDetails({
             <Box direction="column" style={{ width: "100%" }}>
               <Box className={formGroup}>
                 <Typography variant="body2" className={question}>
-                  Email
+                  Full Name
                 </Typography>
                 <TextField
                   id="standard-basic"
                   variant="outlined"
-                  value={form.email}
-                  name="email"
+                  value={form.name}
+                  name="name"
                   onInput={validateInputLength}
                   onChange={handleChange}
                   autoComplete="off"
-                  helperText={errorMessage.email}
+                  // placeholder="Name"
+                  helperText={errorMessage.name}
                   // disabled={true}
                   // className="disabled"
                   fullWidth
@@ -474,28 +475,27 @@ function CustomerDetails({
                   autoComplete="off"
                   // placeholder="Mobile"
                   helperText={errorMessage.mobile}
-                  disabled={true}
-                  className="disabled"
+                  // disabled={true}
+                  // className="disabled"
                   fullWidth
                 />
               </Box>
 
               <Box className={formGroup}>
                 <Typography variant="body2" className={question}>
-                  Full Name
+                  Email
                 </Typography>
                 <TextField
                   id="standard-basic"
                   variant="outlined"
-                  value={form.name}
-                  name="name"
+                  value={form.email}
+                  name="email"
                   onInput={validateInputLength}
                   onChange={handleChange}
                   autoComplete="off"
-                  // placeholder="Name"
-                  helperText={errorMessage.name}
-                  disabled={true}
-                  className="disabled"
+                  helperText={errorMessage.email}
+                  // disabled={true}
+                  // className="disabled"
                   fullWidth
                 />
               </Box>
@@ -516,8 +516,8 @@ function CustomerDetails({
                   // placeholder="Date of Birth"
                   helperText={errorMessage.dob}
                   style={{ width: "100%" }}
-                  disabled={true}
-                  className="disabled"
+                  // disabled={true}
+                  // className="disabled"
                   fullWidth
                 />
               </Box>
@@ -535,8 +535,8 @@ function CustomerDetails({
                   onChange={handleChange}
                   autoComplete="off"
                   helperText={errorMessage.gender}
-                  disabled={true}
-                  className="disabled"
+                  // disabled={true}
+                  // className="disabled"
                   fullWidth
                 />
               </Box>
@@ -554,8 +554,8 @@ function CustomerDetails({
                   autoComplete="off"
                   // placeholder="Date of Birth"
                   helperText={errorMessage.address}
-                  disabled={true}
-                  className="disabled"
+                  // disabled={true}
+                  // className="disabled"
                   fullWidth
                 />
               </Box>
